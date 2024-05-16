a5544e4ab6ce2c62c098eb066e8a0f75905a0667
Chu-rill <churchillrimamskep@gmail.com>
2024-05-16 11:20:33 +0100

    changed

--- src/Data/side.js
+++ src/Data/side.js
@@ -4,48 +4,48 @@ export default [
     link: "/home",
     text: " Home",
     style:
-      "flex hover:cursor-pointer hover:bg-blue-50 text-2xl  hover:rounded-3xl h-12  content-center items-center ",
+      "flex hover:cursor-pointer hover:bg-gray-800 text-2xl  hover:rounded-3xl w-[170px] content-center items-center ",
   },
   {
     img: "fa-solid fa-magnifying-glass",
     link: "/explore",
     text: " Explore",
     style:
-      "flex hover:cursor-pointer hover:bg-blue-50 text-2xl  hover:rounded-3xl h-12  content-center items-center ",
+      "flex hover:cursor-pointer hover:bg-gray-800 text-2xl  hover:rounded-3xl w-[170px] content-center items-center ",
   },
   {
     img: "fa-regular fa-bell",
     link: "/notification",
     text: "  Notification",
     style:
-      "flex hover:cursor-pointer hover:bg-blue-50 text-2xl  hover:rounded-3xl h-12  content-center items-center ",
+      "flex hover:cursor-pointer hover:bg-gray-800 text-2xl  hover:rounded-3xl w-[220px] content-center items-center ",
   },
   {
     img: "fa-regular fa-envelope",
     link: "home",
     text: " Message",
     style:
-      "flex hover:cursor-pointer hover:bg-blue-50 text-2xl  hover:rounded-3xl h-12  content-center items-center ",
+      "flex hover:cursor-pointer hover:bg-gray-800 text-2xl  hover:rounded-3xl w-[200px] content-center items-center ",
   },
   {
     img: "fa-regular fa-bookmark",
     link: "home",
     text: "Bookmarks",
     style:
-      "flex hover:cursor-pointer hover:bg-blue-50 text-2xl  hover:rounded-3xl h-12  content-center items-center ",
+      "flex hover:cursor-pointer hover:bg-gray-800 text-2xl  hover:rounded-3xl w-[200px] content-center items-center ",
   },
   {
     img: "fa-regular fa-user w-5 h-5",
     link: "/profile",
     text: "Profile",
     style:
-      "flex hover:cursor-pointer hover:bg-blue-50 text-2xl  hover:rounded-3xl h-12  content-center items-center ",
+      "flex hover:cursor-pointer hover:bg-gray-800 text-2xl  hover:rounded-3xl w-[150px] content-center items-center ",
   },
   {
     img: "fa-solid fa-ellipsis",
     link: "home",
     text: "More",
     style:
-      "flex hover:cursor-pointer hover:bg-blue-50 text-2xl  hover:rounded-3xl h-12  content-center items-center ",
+      "flex hover:cursor-pointer hover:bg-gray-800 text-2xl  hover:rounded-3xl w-[150px] content-center items-center ",
   },
 ];
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Section(props) {
   return (
     <>
-      <div className={`${props.style} mb-1`}>
-        <div className="p-5 flex">
+      <div className={` mb-1`}>
+        <div className={`${props.style}p-5 flex`}>
           {/* <p className="h-8">{icon}</p> */}
           <i className={`${props.img} mr-2 `}></i>
           <p className="flex items-center w-5 h-5 px-5 text-inherit ">
